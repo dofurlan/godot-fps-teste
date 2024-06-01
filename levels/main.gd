82ae168d3172426256a636b546149fcc91677ad8
dofurlan <dofurlan@gmail.com>
2024-05-31 21:18:24 -0300

    arrumei movimento inimigo

--- levels/main.gd
+++ levels/main.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@export var TEMPO_MAXIMO_TIMEOUT:float = 5.0
+@export var TEMPO_MAXIMO_TIMEOUT:float = 5
 
 var inimigo:PackedScene = load("res://scenes/enemy.tscn")
 
@@ -9,7 +9,7 @@ var limite_Y:int
 var limite_Z:int
 
 func _ready():
-	var meshSize = $floor.mesh.size
+	var meshSize = $NavigationRegion3D/floor.mesh.size
 	
 	limite_X = meshSize.x / 2 - 3
 	limite_Z = meshSize.z / 2 - 3
@@ -17,45 +17,56 @@ func _ready():
 	limite_Y =  meshSize.y 
 		
 func _on_mobtimer_timeout():
-	var time = Time.get_time_dict_from_system()
 	var novo_timeout:float = randf_range(0.0,TEMPO_MAXIMO_TIMEOUT)
 	
 	var newInimigo = inimigo.instantiate()
 	newInimigo.target = $player
 	
 	var nodes = get_children()
-	
+	var tentativa:int = 1
 	while(true):
 		var pos_X:int = randi_range(-limite_X,limite_X)
 		var pos_Z:int = randi_range(-limite_Z,limite_Z)
 		
 		var posicaoInimigo:Vector3 = Vector3(pos_X,limite_Y,pos_Z)
-		newInimigo.position = posicaoInimigo
 		
-		var nodesColisao:Array = []
-		for node in nodes:
-			for child_node in node.get_children():
-				if not 'mesh' in child_node:
-					continue
-				
-				if not child_node.mesh.has_method('get_transformed_aabb'):
-					continue
-
-				if child_node.mesh.get_transformed_aabb().intersects(newInimigo.mesh.get_transformed_aabb()):
-					nodesColisao.append(child_node)
-					break
-				else:
-					print('Again')
-		
-			if nodesColisao.size() > 0:
-				break
-		
-		if nodesColisao.size() == 0:
-			print('%02d:%02d - Spawn at %dx%d - em %fs' % [time.minute, time.second, pos_X, pos_Z, novo_timeout])
+		if !colidiu(nodes,posicaoInimigo):
 			break
 		
-		print('Tentar de novo')
-		
-	add_child(newInimigo)
+		if tentativa >= 3:
+			print("Desistir")
+			break
+			
+		tentativa += 1
+		print('Tentar de novo: %s' % tentativa)
 	
 	$"mob-timer".wait_time =  novo_timeout
+
+func colidiu(nodes:Array[Node],posicaoInimigo:Vector3) -> bool:
+
+	var newInimigo = inimigo.instantiate()
+	newInimigo.target = $player
+	newInimigo.position = posicaoInimigo
+	add_child(newInimigo)
+	var newInimigo_mesh:MeshInstance3D = newInimigo.get_child(1)
+	var newInimigo_aabb:AABB = newInimigo_mesh.get_aabb()
+
+	for node in nodes:
+		for child_node in node.get_children():
+			if not 'mesh' in child_node:
+				continue
+			
+			if not child_node.mesh.has_method('get_aabb'):
+				continue
+			
+			#if child_node.name == 'floor':
+				#continue
+			
+			#var child_node_aabb: AABB = (newInimigo.global_transform.affine_inverse() * child_node.global_transform).xform(child_node.mesh.get_aabb())
+			var child_node_aabb: AABB = child_node.mesh.get_aabb()
+
+			if ((child_node.global_transform.affine_inverse() * newInimigo.global_transform)*newInimigo_aabb).intersects(child_node_aabb):
+				newInimigo.queue_free()
+				return true
+
+	return false
--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -3,6 +3,8 @@ extends CharacterBody3D
 var target:Node3D
 @export var speed:int = 2
 
+@onready var navigation_agent_3d:NavigationAgent3D = $NavigationAgent3D
+
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta):
@@ -15,7 +17,9 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	var direction = Vector3(target.position.x, 0, target.position.z) - position
+	navigation_agent_3d.target_position = target.global_position
+
+	var direction = navigation_agent_3d.get_next_path_position() - position
 	direction = direction.normalized()
 	
 	if direction:
